36f7875df657b2f310f4d47531f0531957978ff3
hurshh <quasarhrsh@gmail.com>
2021-09-06 21:44:02 +0530

    completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
@@ -12,6 +14,43 @@ void main() => runApp(
               ),
             ),
           ),
+          body: magicBall(),
         ),
       ),
     );
+
+class magicBall extends StatefulWidget {
+
+  @override
+  _magicBallState createState() => _magicBallState();
+}
+
+class _magicBallState extends State<magicBall> {
+  @override
+  int ImageNumber =1;
+  randomNumbergenerator(min, max){
+    var rn = new Random();
+    return min+ rn.nextInt(max-min);
+  }
+  Widget build(BuildContext context) {
+
+    return SafeArea(
+      child: Center(
+      child: Center(
+        child: Expanded(
+        child: FlatButton(
+          onPressed: (){
+            setState(() {
+              ImageNumber = randomNumbergenerator(1, 5);
+            });
+          },
+          child: Image(
+            image: AssetImage('images/ball$ImageNumber.png'),
+          ),
+        ),
+    ),
+      ),
+      )
+    );
+  }
+}
